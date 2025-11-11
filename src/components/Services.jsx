084cb7c4dd725ad126bfc06ff7fcd6b42beabc4f
Camilo <caop.decode@gmail.com>
2025-11-11 12:45:46 -0500

    Star Asegurados Web V2

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import ModalService from "./ModalServices";
+import serviceImage from "../assets/IMGs/Services.png";
 
 const Services = () => {
   const [selected, setSelected] = useState(null);
@@ -22,26 +23,32 @@ const Services = () => {
       whileInView={{ opacity: 1 }}
       transition={{ duration: 0.8 }}
     >
-      <div className="services-card image-card">IMAGEN AQUÍ</div>
+      <div className="services-card image-card"><img src={serviceImage} alt="" /></div>
       <div className="services-card list-card">
-        <h2>NUESTROS PRODUCTOS</h2>
-        <p>¡Selecciona uno y conoce más!</p>
-        <h3>Seguros</h3>
-        <ul>
-          {servicesSeg.map((s) => (
-            <li key={s}>
-              <button onClick={() => setSelected(s)}>{s}</button>
-            </li>
-          ))}
-        </ul>
-        <h3>Créditos</h3>
-        <ul>
-            {servicesFin.map((c) => (
-                <li key={c}>
-                    <button onClick={() => setSelected(c)}>{c}</button>
-                </li>
+        <div className="services-card-title">
+          <h2>NUESTROS PRODUCTOS</h2>
+          <p>¡Selecciona uno y conoce más!</p>
+        </div>
+        <div className="seguros-services">
+          <h3>Seguros</h3>
+          <ul className="seguros-list">
+            {servicesSeg.map((s) => (
+              <li key={s}>
+                <button onClick={() => setSelected(s)}>{s}</button>
+              </li>
             ))}
-        </ul>
+          </ul>
+        </div>
+        <div className="creditos-services">
+          <h3>Créditos</h3>
+          <ul className="creditos-list">
+              {servicesFin.map((c) => (
+                  <li key={c}>
+                      <button onClick={() => setSelected(c)}>{c}</button>
+                  </li>
+              ))}
+          </ul>
+        </div>
       </div>
 
       <AnimatePresence>
